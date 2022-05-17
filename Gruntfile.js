519f0a42ae1d1a1f2095efee44be4e4e529297c1
xal3xx <45608602+xal3xx@users.noreply.github.com>
2022-05-17 12:01:32 +0200

    added some last changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,57 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+            png: {
+                options: {
+                    optimizationLevel: 7
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/',
+                        src: ['**/*.png'],
+                        dest: 'public/images_min/',
+                        ext: '.png'
+                    }
+                ]
+            },
+            jpg: {
+                options: {
+                    progressive: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/',
+                        src: ['**/*.jpg','**/*.jpeg'],
+                        dest: 'public/images_min/',
+                        ext: '.jpg'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'public/html_min/index.html': 'index.html',
+                    'public/html_min/menu.html': 'menu.html',
+                    'public/html_min/menu-empty.html': 'menu-empty.html',
+                    'public/html_min/newsletter.html': 'newsletter.html',
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass', 'imagemin', 'htmlmin']);
 
 };
